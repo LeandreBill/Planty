2da029a4704fa69a7218c5cc3c4b4eef0d36bb9f
leandre <leandredeck@gmail.com>
2023-05-17 18:11:39 +0400

    3eme page à terminer

---
+++ wp-content/themes/oceanwp/templates/Commander.php
@@ -0,0 +1,295 @@
+<?php
+/**
+ * Template Name: Commander
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+
+<!DOCTYPE html>
+<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	</head>
+
+    <header>
+		<?php wp_head(); ?>
+        <header id="site-header" class="minimal-header effect-one clr" data-height="89" itemscope="itemscope" itemtype="https://schema.org/WPHeader" role="banner">
+
+	
+					
+			<div id="site-header-inner" class="clr container">
+
+				
+				
+
+<div id="site-logo" class="clr" itemscope="" itemtype="https://schema.org/Brand">
+
+	
+	<div id="site-logo-inner" class="clr">
+
+		<a href="http://planty/" class="custom-logo-link" rel="home" aria-current="page"><img width="2560" height="668" src="http://planty/wp-content/uploads/2023/05/planty-scaled.jpg" class="custom-logo" alt="Planty" decoding="async" srcset="http://planty/wp-content/uploads/2023/05/planty-scaled.jpg 2560w, http://planty/wp-content/uploads/2023/05/planty-300x78.jpg 300w, http://planty/wp-content/uploads/2023/05/planty-1024x267.jpg 1024w, http://planty/wp-content/uploads/2023/05/planty-768x200.jpg 768w, http://planty/wp-content/uploads/2023/05/planty-1536x401.jpg 1536w, http://planty/wp-content/uploads/2023/05/planty-2048x534.jpg 2048w" sizes="(max-width: 2560px) 100vw, 2560px"></a>
+	</div><!-- #site-logo-inner -->
+
+	
+	
+</div><!-- #site-logo -->
+
+			<div id="site-navigation-wrap" class="no-top-border clr">
+			
+			
+			
+			<nav id="site-navigation" class="navigation main-navigation clr" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement" role="navigation">
+
+				<ul id="menu-planty" class="main-menu dropdown-menu sf-menu"><li id="menu-item-15" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15"><a href="http://planty/nous-contacter/" class="menu-link"><span class="text-wrap">Nous Rencontrer</span></a></li><li id="menu-item-16" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-16"><a href="#" class="menu-link"><span class="text-wrap">Admin</span></a></li><li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="http://planty/commander/" class="menu-link"><span class="text-wrap">Commander</span></a></li></ul>
+			</nav><!-- #site-navigation -->
+
+			
+			
+					</div><!-- #site-navigation-wrap -->
+			
+		
+	
+				
+	
+	<div class="oceanwp-mobile-menu-icon clr mobile-right">
+
+		
+		
+		
+		<a href="http://planty/#mobile-menu-toggle" class="mobile-menu" aria-label="Menu mobile">
+							<i class="fa fa-bars" aria-hidden="true"></i>
+								<span class="oceanwp-text">Menu</span>
+				<span class="oceanwp-close-text">Fermer</span>
+						</a>
+
+		
+		
+		
+	</div><!-- #oceanwp-mobile-menu-navbar -->
+
+	
+
+			</div><!-- #site-header-inner -->
+
+			
+<div id="mobile-dropdown" class="clr" style="box-sizing: border-box; display: none;">
+
+	<nav class="clr" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement">
+
+		<ul id="menu-planty-1" class="menu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15"><a href="http://planty/nous-contacter/">Nous Rencontrer</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-16"><a href="#">Admin</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="http://planty/commander/">Commander</a></li>
+</ul>
+<div id="mobile-menu-search" class="clr">
+	<form aria-label="Rechercher sur ce site" method="get" action="http://planty/" class="mobile-searchform">
+		<input aria-label="Insert search query" value="" class="field" id="ocean-mobile-search-1" type="search" name="s" autocomplete="off" placeholder="Rechercher">
+		<button aria-label="Envoyer la recherche" type="submit" class="searchform-submit">
+			<i class=" icon-magnifier" aria-hidden="true" role="img"></i>		</button>
+					</form>
+</div><!-- .mobile-menu-search -->
+
+	</nav>
+
+</div>
+
+			
+				
+</header>
+
+
+	<!-- Begin Body -->
+<body>
+    <main id="main" class="site-main clr" role="main" style="background-color:#326238">
+
+				
+	
+	<div id="content-wrap" class="container-clr-bis">
+
+		
+		<div id="primary" class="content-area clr">
+
+			
+			<div id="content" class="site-content clr">
+
+				
+				
+<article class="single-page-article clr">
+
+	
+<div class="entry clr" itemprop="text">
+
+	
+	
+<p class="has-text-align-center has-white-color has-text-color" style="font-size:38px;font-style:normal;font-weight:800">COMMANDER</p>
+
+
+
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+
+<hr class="wp-block-separator alignwide has-text-color has-white-color has-alpha-channel-opacity has-white-background-color has-background is-style-wide">
+
+
+
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+
+<p class="has-white-color has-text-color" style="font-size:30px">Votre Commande</p>
+
+
+
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+
+<div class="wp-block-group alignwide is-layout-constrained"><div class="wp-block-group__inner-container">
+<div class="wp-block-columns alignwide are-vertically-aligned-center is-layout-flex wp-container-6">
+<div class="wp-block-column is-vertically-aligned-center is-layout-flow" style="flex-basis:25%">
+<figure class="wp-block-image size-full"><img decoding="async" width="393" height="262" src="http://planty/wp-content/uploads/2023/05/Group-12.png" alt="" class="wp-image-326" srcset="http://planty/wp-content/uploads/2023/05/Group-12.png 393w, http://planty/wp-content/uploads/2023/05/Group-12-300x200.png 300w" sizes="(max-width: 393px) 100vw, 393px"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-vertically-aligned-center is-layout-flow" style="flex-basis:25%">
+<figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="394" height="263" src="http://planty/wp-content/uploads/2023/05/Group-13.png" alt="" class="wp-image-327" srcset="http://planty/wp-content/uploads/2023/05/Group-13.png 394w, http://planty/wp-content/uploads/2023/05/Group-13-300x200.png 300w" sizes="(max-width: 394px) 100vw, 394px"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-vertically-aligned-center is-layout-flow" style="flex-basis:25%">
+<figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="393" height="262" src="http://planty/wp-content/uploads/2023/05/Group-15.png" alt="" class="wp-image-328" srcset="http://planty/wp-content/uploads/2023/05/Group-15.png 393w, http://planty/wp-content/uploads/2023/05/Group-15-300x200.png 300w" sizes="(max-width: 393px) 100vw, 393px"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-vertically-aligned-center is-layout-flow" style="flex-basis:25%">
+<figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="393" height="262" src="http://planty/wp-content/uploads/2023/05/Group-14.png" alt="" class="wp-image-329" srcset="http://planty/wp-content/uploads/2023/05/Group-14.png 393w, http://planty/wp-content/uploads/2023/05/Group-14-300x200.png 300w" sizes="(max-width: 393px) 100vw, 393px"></figure>
+</div>
+</div>
+
+
+
+<div class="wp-block-columns alignwide are-vertically-aligned-top is-layout-flex wp-container-23">
+<div class="wp-block-column is-vertically-aligned-top is-layout-flow">
+<div class="wp-block-columns is-layout-flex wp-container-9">
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+</div>
+</div>
+
+
+
+<div class="wp-block-column is-vertically-aligned-top is-layout-flow">
+<div class="wp-block-columns is-layout-flex wp-container-13">
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+</div>
+</div>
+
+
+
+<div class="wp-block-column is-vertically-aligned-top is-layout-flow">
+<div class="wp-block-columns is-layout-flex wp-container-17">
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+</div>
+</div>
+
+
+
+<div class="wp-block-column is-vertically-aligned-top is-layout-flow">
+<div class="wp-block-columns is-layout-flex wp-container-21">
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+
+
+
+<div class="wp-block-column is-layout-flow">
+<figure class="wp-block-image size-full is-resized"><img decoding="async" loading="lazy" src="http://planty/wp-content/uploads/2023/05/Bouton.png" alt="" class="wp-image-361" width="100" height="56"></figure>
+</div>
+</div>
+</div>
+</div>
+</div></div>
+
+	
+</div>
+
+</article>
+
+				
+			</div><!-- #content -->
+
+			
+		</div><!-- #primary -->
+
+		
+	</div><!-- #content-wrap -->
+
+	
+
+	</main>
+	</body>
+
+
+    <footer>
+		<?php wp_footer(); ?>
+        <footer id="footer" class="site-footer" itemscope="itemscope" itemtype="https://schema.org/WPFooter" role="contentinfo">
+
+	
+	<div id="footer-inner" class="clr">
+
+		
+
+<div id="footer-bottom" class="clr no-footer-nav">
+
+	
+	<div id="footer-bottom-inner" class="container clr">
+
+		
+		
+			<div id="copyright" class="clr" role="contentinfo">
+				Mentions légales			</div><!-- #copyright -->
+
+			
+	</div><!-- #footer-bottom-inner -->
+
+	
+</div><!-- #footer-bottom -->
+
+
+	</div><!-- #footer-inner -->
+
+	
+</footer>
+
+
+</html>
